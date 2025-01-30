f55889eae37b389fd2a16dafd3bf5f274a632e1f
volodya2001 <volodya01volodya@gmail.com>
2025-01-30 20:27:39 +0400

    Add files via upload

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -5,8 +5,8 @@ export default function Select({ handleSelectChange }) {
             <label>Select a show:</label>
 
             <select id="movie" onChange={handleSelectChange}>
-                <option value="320">STAND UP տակ (29.01 / 17:00)</option>
-                <option value="220">STAND UP տակ (12.02 / 17:00)</option>
+                <option value="320" disabled>STAND UP տակ (29.01 / 17:00)</option>
+                <option value="220" selected>STAND UP տակ (12.02 / 17:00)</option>
             </select>
         </div>
     )
